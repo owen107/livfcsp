0f0c78a0d068be24fcf869a00d19331ec0f7fabb
owen107 <owenxu107@gmail.com>
2015-03-21 20:06:20 -0400

    Finished styling all the sections

--- builds/development/js/myscript.js
+++ builds/development/js/myscript.js
@@ -114,7 +114,28 @@ $(function() {
         mobile:       true
     }).init();
 
+    // Initiate tooltip plugin
     $("[data-toggle=tooltip]").tooltip();
+
+    // Check to see if the window is top if not then display button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 800) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+
+    // Click event to scroll to top
+    $('.scrollToTop').click(function () {
+        $('html, body').animate({scrollTop: 0}, 800);
+        return false;
+    });
     
 
+    $(function(){
+		$.stellar({
+			horizontalScrolling: false
+		});
+	});
 });
